c0b2aa74f394ea18e3ed8a7f75f4116fe9bfebd9
chen <1718325213@qq.com>
2021-03-09 11:34:33 +0800

    完成了用户列表功能开发

---
+++ src/components/Welecome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>Welecome</h3>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welecome from '../components/Welecome.vue'
+import Users from '../components/Users/user.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welecome',
+      children: [
+        { path: '/welecome', component: Welecome },
+        { path: '/users', component: Users }
+      ]
+    }
   ]
 })
 // 挂载路由导航守卫
